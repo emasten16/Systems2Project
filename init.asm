875586d51f61e18987350874656cdc48cba82123
Julia Edholm <dramagirl1012@yahoo.com>
2015-04-23 11:46:07 -0400

    calling init/sysc

---
+++ init.asm
@@ -0,0 +1,35 @@
+.Code
+
+;;; The entry point.
+__start:
+
+;;question. will our processes be able to use functions. I assume so, but how?
+
+;;print a message to the console
+    COPY    %G0  *+_print_sysc_code
+    COPY    %G1    +_string_start_init_msg
+    SYSC
+
+    ; Copy one of the source values into a register.
+    COPY	%G0	*+x
+    ADD	%G1	%G0	*+y
+
+
+    
+	;; Halt the processor.
+end:	HALT
+
+.Numeric
+;; The source values to be added.
+x:	5
+y:	-3
+
+;;SYSC codes
+_exit_sysc_code: 1
+_create_sysc_code: 2 
+_get_rom_count_sysc_code: 3
+_print_sysc_code: 4
+
+.Text
+_string_blank_line: "                                                                                "
+_string_start_init_msg: "init has started\n"
--- kernelJuliaBaseJuliaEdits.asm
+++ kernelJuliaBaseJuliaEdits.asm
@@ -18,7 +18,7 @@ __start:
     COPY    *+INVALID_INSTRUCTION    +Dummy_Handler
     COPY    *+PERMISSION_VIOLATION     +Dummy_Handler
     COPY    *+INVLID_SHIFT_AMOUNT     +Dummy_Handler
-    COPY    *+SYSTEM_CALL     +Dummy_Handler
+    COPY    *+SYSTEM_CALL     +SYSC_Handler
     COPY    *+INVALID_DEVICE_VALUE    +Dummy_Handler
     COPY    *+DEVICE_FAILURE     +Dummy_Handler
 
@@ -124,35 +124,39 @@ main:
     
 ;;MAIN METHOD DOES STUFF
 
-;;caller prolog for a test function:
-    SUBUS   %SP     %SP     12; move SP over 3 words
+
+;;caller prolog for the print function function:
+    SUBUS   %SP     %SP     8; move SP over 2 words because no return value
     COPY    *%SP    %FP; presrve FP in the PFP word
-    ADDUS   %FP     %SP     4; %FP has address for word RA
+    ADDUS   %G5     %SP     4; %G5 has address for word RA
     SUBUS   %SP     %SP     4; %SP has address of first Argument
-    COPY    *%SP    3; 3 =  the argument that I will pass to the test function
-    CALL   +function_test  *%FP
+    COPY    *%SP    +_string_main_method_msg; the argument that I will pass to the test function
+    COPY    %FP     %SP
+    CALL   +_procedure_print  *%G5
 
 ;;caller epilogue
-    ADDUS   %SP     %SP     4; pop the arguments. %SP now points to PFP
-    COPY    %FP     *%SP
-    ADDUS   %SP     %SP     8; %SP now points to the return value
-    
-    COPY    %G5     *%SP; %G5 has the value that was returned from test function
-
-
-
-   COPY     %G1     0 ;let %G1 be the counter for how many rom type devices have been seen
-   ;;TASK 2: Find and run init.asm
-start_loop: BEQ    +rom_type_device         *%G0       *+_static_ROM_device_code
-    ADD     %G0    %G0  *+_skip_process_table_element
-    JUMP    +start_loop
-
-rom_type_device:
-    ;; add one to the count of total rom type devices seen
-    ADD     %G1     %G1     1
-    BEQ    +deal_with_process1 %G1 3; branch if this is third ROM Device (1= bios, 2 = kernel, 3 = init)
-    ADD     %G0    %G0  *+_skip_process_table_element
-    JUMP     +start_loop          
+    ADDUS       %SP     %SP     4       ; Pop arg[0]
+    COPY        %FP     *%SP                ; %FP = pfp
+    ADDUS       %SP     %SP     8       ; Pop pfp / ra
+ 
+ ;;TASK 2: Find and run init.asm
+ 
+;;caller prolog for the find_device prcedure
+    SUBUS       %SP     %SP     12      ; Push pfp / ra / rv
+    COPY        *%SP    %FP             ; pFP = %FP
+    SUBUS       %SP     %SP     4       ; Push arg[1]
+    COPY        *%SP    3               ; Find the 3st device of the given type.
+    SUBUS       %SP     %SP     4       ; Push arg[0]
+    COPY        *%SP   *+_static_ROM_device_code   ; Find a console device.
+    COPY        %FP     %SP             ; Update %FP
+    ADDUS       %G5     %SP     12      ; %G5 = &ra
+    CALL        +_procedure_find_device     *%G5
+    ;;caller epilog
+    ADDUS       %SP     %SP     8       ; Pop arg[0,1]
+    COPY        %FP     *%SP                ; %FP = pfp
+    ADDUS       %SP     %SP     8       ; Pop pfp / ra
+    COPY       %G0     *%SP                ; %G0 = &dt[console]= the address of init process in bus
+    ADDUS       %SP     %SP     4       ; Pop rv
     
 deal_with_process1:
    ;;make a process table!!!!
@@ -254,6 +258,316 @@ function_test:
     
 Dummy_Handler:
     HALT
+
+SYSC_Handler:
+;;; %G0 holds 1 if EXIT, 2 if CREATE, 3 if GET_ROM_COUNT, 4 if PRINT
+   ;;;BEQ     +EXIT_Handler   %G0     *+_exit_sysc_code
+    ;;;BEQ     +CREATE_Handler  %G0     *+_create_sysc_code
+   ;;; BEQ     +GET_ROM_COUNT_Handler   %G0     *+_get_rom_count_sysc_code
+    BEQ     +PRINT_Handler          %G0     *+_print_sysc_code
+
+EXIT_Handler:
+;;return process memory to free space
+;;search process table for process ID,  make it 0
+;;schedule a new process
+
+GET_ROM_COUNT_Handler:
+;;return the number of ROMs available in the system not including the bios and kernel
+;;jump back to where you were
+
+PRINT_Handler:
+;;caller prolog for the print function function:
+    SUBUS   %SP     %SP     8; move SP over 2 words because no return value
+    COPY    *%SP    %FP; presrve FP in the PFP word
+    ADDUS   %G5     %SP     4; %G5 has address for word RA
+    SUBUS   %SP     %SP     4; %SP has address of first Argument
+    COPY    *%SP    *%G1; the argument that I will pass to the print. When the SYSC happens, user stores 4 in G0 to call a print sysc and a pointer to the string in G1
+    COPY    %FP     %SP
+    CALL   +_procedure_print  *%G5
+
+;;caller epilogue
+    ADDUS       %SP     %SP     4       ; Pop arg[0]
+    COPY        %FP     *%SP                ; %FP = pfp
+    ADDUS       %SP     %SP     8       ; Pop pfp / ra
+
+;;after printing, we want to jump back into the process we were in when print was called
+;;************************************************************************************************
+
+;;; ================================================================================================================================
+;;; Procedure: print
+;;; Callee preserved registers:
+;;;   [%FP - 4]: G0
+;;;   [%FP - 8]: G3
+;;;   [%FP - 12]: G4
+;;; Parameters:
+;;;   [%FP + 0]: A pointer to the beginning of a null-terminated string.
+;;; Caller preserved registers:
+;;;   [%FP + 4]: FP
+;;; Return address:
+;;;   [%FP + 8]
+;;; Return value:
+;;;   <none>
+;;; Locals:
+;;;   %G0: Pointer to the current position in the string.
+    
+_procedure_print:
+
+    ;; Prologue: Push preserved registers.
+    SUBUS       %SP     %SP     4
+    COPY        *%SP        %G0
+    SUBUS       %SP     %SP     4
+    COPY        *%SP        %G3
+    SUBUS       %SP     %SP     4
+    COPY        *%SP        %G4
+
+    COPY    %G5     100 ;test that we made it 
+    ;; If not yet initialized, set the console base/limit statics.
+    BNEQ        +print_init_loop    *+_static_console_base      0
+    
+    ;;caller prolog for the find_device prcedure
+    SUBUS       %SP     %SP     12      ; Push pfp / ra / rv
+    COPY        *%SP    %FP             ; pFP = %FP
+    SUBUS       %SP     %SP     4       ; Push arg[1]
+    COPY        *%SP    1               ; Find the 1st device of the given type.
+    SUBUS       %SP     %SP     4       ; Push arg[0]
+    COPY        *%SP   *+_static_console_device_code   ; Find a console device.
+    COPY        %FP     %SP             ; Update %FP
+    ADDUS       %G5     %SP     12      ; %G5 = &ra
+    CALL        +_procedure_find_device     *%G5
+    ;;caller epilog
+    ADDUS       %SP     %SP     8       ; Pop arg[0,1]
+    COPY        %FP     *%SP                ; %FP = pfp
+    ADDUS       %SP     %SP     8       ; Pop pfp / ra
+    COPY       %G4     *%SP                ; %G4 = &dt[console]
+    ADDUS       %SP     %SP     4       ; Pop rv
+
+    ;; Panic if the console was not found.
+    BNEQ        +print_found_console    %G4     0
+    COPY        %G5     *+_static_kernel_error_console_not_found
+    HALT
+    
+print_found_console:    
+    ADDUS       %G3     %G4     *+_static_dt_base_offset  ; %G3 = &console[base]
+    COPY        *+_static_console_base      *%G3              ; Store static console[base]
+    ADDUS       %G3     %G4     *+_static_dt_limit_offset ; %G3 = &console[limit]
+    COPY        *+_static_console_limit     *%G3              ; Store static console[limit]
+    
+print_init_loop:    
+
+    ;; Loop through the characters of the given string until the null character is found.
+    COPY        %G0     *%FP                ; %G0 = str_ptr
+print_loop_top:
+    COPYB       %G4     *%G0                ; %G4 = current_char
+
+    ;; The loop should end if this is a null character
+    BEQ     +print_loop_end %G4     0
+
+    ;; Scroll without copying the character if this is a newline.
+    COPY        %G3     *+_static_newline_char      ; %G3 = <newline>
+    BEQ     +print_scroll_call  %G4 %G3
+
+    ;; Assume that the cursor is in a valid location.  Copy the current character into it.
+    ;; The cursor position c maps to buffer location: console[limit] - width + c
+    SUBUS       %G3     *+_static_console_limit *+_static_console_width    ; %G3 = console[limit] - width
+    ADDUS       %G3     %G3     *+_static_cursor_column        ; %G3 = console[limit] - width + c
+    COPYB       *%G3        %G4                        ; &(height - 1, c) = current_char
+    
+    ;; Advance the cursor, scrolling if necessary.
+    ADD     *+_static_cursor_column *+_static_cursor_column     1   ; c = c + 1
+    BLT     +print_scroll_end   *+_static_cursor_column *+_static_console_width ; Skip scrolling if c < width
+    ;; Fall through...
+    
+print_scroll_call:  
+    SUBUS       %SP     %SP     8               ; Push pfp / ra
+    COPY        *%SP        %FP                     ; pfp = %FP
+    COPY        %FP     %SP                     ; %FP = %SP
+    ADDUS       %G5     %FP     4               ; %G5 = &ra
+    CALL        +_procedure_scroll_console  *%G5
+    COPY        %FP     *%SP                        ; %FP = pfp
+    ADDUS       %SP     %SP     8               ; Pop pfp / ra
+
+print_scroll_end:
+    ;; Place the cursor character in its new position.
+    SUBUS       %G3     *+_static_console_limit     *+_static_console_width ; %G3 = console[limit] - width
+    ADDUS       %G3     %G3     *+_static_cursor_column         ; %G3 = console[limit] - width + c  
+    COPY        %G4     *+_static_cursor_char                       ; %G4 = <cursor>
+    COPYB       *%G3        %G4                         ; console@cursor = <cursor>
+    
+    ;; Iterate by advancing to the next character in the string.
+    ADDUS       %G0     %G0     1
+    JUMP        +print_loop_top
+
+print_loop_end:
+    ;; Epilogue: Pop and restore preserved registers, then return.
+    COPY        %G4     *%SP
+    ADDUS       %SP     %SP     4
+    COPY        %G3     *%SP
+    ADDUS       %SP     %SP     4
+    COPY        %G0     *%SP
+    ADDUS       %SP     %SP     4
+    ADDUS       %G5     %FP     8       ; %G5 = &ra
+    JUMP        *%G5
+
+;;; Procedure: scroll_console
+;;; Description: Scroll the console and reset the cursor at the 0th column.
+;;; Callee reserved registers:
+;;;   [%FP - 4]:  G0
+;;;   [%FP - 8]:  G1
+;;;   [%FP - 12]: G4
+;;; Parameters:
+;;;   <none>
+;;; Caller preserved registers:
+;;;   [%FP + 0]:  FP
+;;; Return address:
+;;;   [%FP + 4]
+;;; Return value:
+;;;   <none>
+;;; Locals:
+;;;   %G0:  The current destination address.
+;;;   %G1:  The current source address.
+
+_procedure_scroll_console:
+
+    ;; Prologue: Push preserved registers.
+    SUBUS       %SP     %SP     4
+    COPY        *%SP        %G0
+    SUBUS       %SP     %SP     4
+    COPY        *%SP        %G1
+    SUBUS       %SP     %SP     4
+    COPY        *%SP        %G4
+
+    ;; Initialize locals.
+    COPY        %G0     *+_static_console_base             ; %G0 = console[base]
+    ADDUS       %G1     %G0     *+_static_console_width    ; %G1 = console[base] + width
+
+    ;; Clear the cursor.
+    SUBUS       %G4     *+_static_console_limit     *+_static_console_width ; %G4 = console[limit] - width
+    ADDUS       %G4     %G4     *+_static_cursor_column         ; %G4 = console[limit] - width + c
+    COPYB       *%G4        *+_static_space_char                    ; Clear cursor.
+
+    ;; Copy from the source to the destination.
+    ;;   %G3 = DMA portal
+    ;;   %G4 = DMA transfer length
+    ADDUS       %G3     8       *+_static_device_table_base ; %G3 = &controller[limit]
+    SUBUS       %G3     *%G3        12                          ; %G3 = controller[limit] - 3*|word| = &DMA_portal
+    SUBUS       %G4     *+_static_console_limit %G0             ; %G4 = console[base] - console[limit] = |console|
+    SUBUS       %G4     %G4     *+_static_console_width     ; %G4 = |console| - width
+
+    ;; Copy the source, destination, and length into the portal.  The last step triggers the DMA copy.
+    COPY        *%G3        %G1                     ; DMA[source] = console[base] + width
+    ADDUS       %G3     %G3     4           ; %G3 = &DMA[destination]
+    COPY        *%G3        %G0                     ; DMA[destination] = console[base]
+    ADDUS       %G3     %G3     4           ; %G3 = &DMA[length]
+    COPY        *%G3        %G4                     ; DMA[length] = |console| - width; DMA trigger
+
+    ;; Perform a DMA transfer to blank the last line with spaces.
+    SUBUS       %G3     %G3     8           ; %G3 = &DMA_portal
+    COPY        *%G3        +_string_blank_line         ; DMA[source] = &blank_line
+    ADDUS       %G3     %G3     4           ; %G3 = &DMA[destination]
+    SUBUS       *%G3        *+_static_console_limit *+_static_console_width ; DMA[destination] = console[limit] - width
+    ADDUS       %G3     %G3     4           ; %G3 = &DMA[length]
+    COPY        *%G3        *+_static_console_width         ; DMA[length] = width; DMA trigger
+    
+    ;; Reset the cursor position.
+    COPY        *+_static_cursor_column     0                           ; c = 0
+    SUBUS       %G4     *+_static_console_limit     *+_static_console_width ; %G4 = console[limit] - width
+    COPYB       *%G4        *+_static_cursor_char                   ; Set cursor.
+    
+    ;; Epilogue: Pop and restore preserved registers, then return.
+    COPY        %G4     *%SP
+    ADDUS       %SP     %SP     4
+    COPY        %G1     *%SP
+    ADDUS       %SP     %SP     4
+    COPY        %G0     *%SP
+    ADDUS       %SP     %SP     4
+    ADDUS       %G5     %FP     4       ; %G5 = &ra
+    JUMP        *%G5
+;;; ================================================================================================================================
+ ;;; ================================================================================================================================   
+;;; Procedure: find_device
+;;; Callee preserved registers:
+;;;   [%FP - 4]:  G0
+;;;   [%FP - 8]:  G1
+;;;   [%FP - 12]: G2
+;;;   [%FP - 16]: G4
+;;; Parameters:
+;;;   [%FP + 0]: The device type to find.
+;;;   [%FP + 4]: The instance of the given device type to find (e.g., the 3rd ROM).
+;;; Caller preserved registers:
+;;;   [%FP + 8]:  FP
+;;; Return address:
+;;;   [%FP + 12]
+;;; Return value:
+;;;   [%FP + 16]: If found, a pointer to the correct device table entry; otherwise, null.
+;;; Locals:
+;;;   %G0: The device type to find (taken from parameter for convenience).
+;;;   %G1: The instance of the given device type to find. (from parameter).
+;;;   %G2: The current pointer into the device table.
+
+_procedure_find_device:
+
+    ;; Prologue: Preserve the registers used on the stack.
+    SUBUS       %SP     %SP     4
+    COPY        *%SP    %G0
+    SUBUS       %SP     %SP     4
+    COPY        *%SP    %G1
+    SUBUS       %SP     %SP     4
+    COPY        *%SP    %G2
+    SUBUS       %SP     %SP     4
+    COPY        *%SP    %G4
+    
+    ;; Initialize the locals.
+    COPY        %G0     *%FP
+    ADDUS       %G1     %FP     4
+    COPY        %G1     *%G1
+    COPY        %G2     *+_static_device_table_base
+    
+find_device_loop_top:
+
+    ;; End the search with failure if we've reached the end of the table without finding the device.
+    BEQ     +find_device_loop_failure   *%G2        *+_static_none_device_code
+
+    ;; If this entry matches the device type we seek, then decrement the instance count.  If the instance count hits zero, then
+    ;; the search ends successfully.
+    BNEQ        +find_device_continue_loop  *%G2        %G0
+    SUB     %G1             %G1     1
+    BEQ     +find_device_loop_success   %G1     0
+    
+find_device_continue_loop:  
+
+    ;; Advance to the next entry.
+    ADDUS       %G2         %G2     *+_static_dt_entry_size
+    JUMP        +find_device_loop_top
+
+find_device_loop_failure:
+
+    ;; Set the return value to a null pointer.
+    ADDUS       %G4         %FP     16  ; %G4 = &rv
+    COPY        *%G4            0           ; rv = null
+    JUMP        +find_device_return
+
+find_device_loop_success:
+
+    ;; Set the return pointer into the device table that currently points to the given iteration of the given type.
+    ADDUS       %G4         %FP     16  ; %G4 = &rv
+    COPY        *%G4        %G2         ; rv = &dt[<device>]
+    ;; Fall through...
+    
+find_device_return:
+
+    ;; Epilogue: Restore preserved registers, then return.
+    COPY        %G4     *%SP
+    ADDUS       %SP     %SP     4
+    COPY        %G2     *%SP
+    ADDUS       %SP     %SP     4
+    COPY        %G1     *%SP
+    ADDUS       %SP     %SP     4
+    COPY        %G0     *%SP
+    ADDUS       %SP     %SP     4
+    ADDUS       %G5     %FP     12  ; %G5 = &ra
+    JUMP        *%G5
+;;; ================================================================================================================================
+   
     
 .Numeric
 end_of_bus: 0
@@ -262,6 +576,9 @@ _static_device_table_base:	0x00001000
 _incriment_by_one_word:       0x00000004
 _incriment_by_two_words:   0x00000008
 _skip_process_table_element:       0x0000000c
+_static_dt_entry_size:      12
+_static_dt_base_offset:     4
+_static_dt_limit_offset:    8
 _static_none_device_code:	0
 _static_controller_device_code:	1
 _static_ROM_device_code:	2
@@ -272,7 +589,14 @@ _static_console_device_code:	4
 _static_min_RAM_KB:		64
 _static_bytes_per_KB:		1024
 _static_bytes_per_page:		4096	; 4 KB/page
-_static_kernel_KB_size:		32	; KB taken by the kerne
+_static_kernel_KB_size:		32	; KB taken by the kernel  
+
+;; Constants for printing and console management.
+_static_console_width:      80
+_static_console_height:     24
+_static_space_char:     0x20202020 ; Four copies for faster scrolling.  If used with COPYB, only the low byte is used.
+_static_cursor_char:        0x5f
+_static_newline_char:       0x0a
 
 	;; Error codes.
 _static_kernel_error_RAM_not_found:	0xffff0001
@@ -288,6 +612,12 @@ _static_console_base:		0
 _static_console_limit:		0
 _static_kernel_base:		0
 _static_kernel_limit:		0
+
+;;SYSC codes
+_exit_sysc_code: 1
+_create_sysc_code: 2 
+_get_rom_count_sysc_code: 3
+_print_sysc_code: 4
 
 ;Trap Table
 TT_BASE:
@@ -307,3 +637,86 @@ DEVICE_FAILURE: 0
 ;Interrupt buffer register
 Interrupt_buffer_IP: 0
 INterrupt_buffer_MISC: 0
+
+;;;PROCESS TABLE - room for five processes
+process_table:
+entry0_process_ID:  0
+entry0_base:    0
+entry0_limit:   0
+entry0_IP:  0
+entry0_G0:  0
+entry0_G1:  0
+entry0_G2:  0
+entry0_G3:  0
+entry0_G4:  0
+entry0_G5:  0
+entry0_SP:  0
+entry0_FP:  0
+entry1_process_ID:  0
+entry1_base:    0
+entry1_limit:   0
+entry1_IP:  0
+entry1_G0:  0
+entry1_G1:  0
+entry1_G2:  0
+entry1_G3:  0
+entry1_G4:  0
+entry1_G5:  0
+entry1_SP:  0
+entry1_FP:  0
+entry2_process_ID:  0
+entry2_base:    0
+entry2_limit:   0
+entry2_IP:  0
+entry2_G0:  0
+entry2_G1:  0
+entry2_G2:  0
+entry2_G3:  0
+entry2_G4:  0
+entry2_G5:  0
+entry2_SP:  0
+entry2_FP:  0
+entry3_process_ID:  0
+entry3_base:    0
+entry3_limit:   0
+entry3_IP:  0
+entry3_G0:  0
+entry3_G1:  0
+entry3_G2:  0
+entry3_G3:  0
+entry3_G4:  0
+entry3_G5:  0
+entry3_SP:  0
+entry3_FP:  0
+entry4_process_ID:  0
+entry4_base:    0
+entry4_limit:   0
+entry4_IP:  0
+entry4_G0:  0
+entry4_G1:  0
+entry4_G2:  0
+entry4_G3:  0
+entry4_G4:  0
+entry4_G5:  0
+entry4_SP:  0
+entry4_FP:  0
+entry5_process_ID:  0
+entry5_base:    0
+entry5_limit:   0
+entry5_IP:  0
+entry5_G0:  0
+entry5_G1:  0
+entry5_G2:  0
+entry5_G3:  0
+entry5_G4:  0
+entry5_G5:  0
+entry5_SP:  0
+entry5_FP:  0
+end_of_process_table:   27
+;;;this is so that we can check to see if we have reached the end of the process table and it is full
+
+.Text
+_string_blank_line: "                                                                                "
+_string_test_msg: "test message\n"
+_string_done_msg:	"done.\n"
+_string_main_method_msg: "Main method has started.\n"
